1dd80b3bc6a0c4d3e52626e3654ea3849df56ae5
Kvelasquez92 <k.kava_k@outlook.com>
2016-07-05 00:22:10 -0600

    Se actualizó el config.py según lo indicado en el archivo README

--- source/conf.py
+++ source/conf.py
@@ -46,8 +46,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Documentación de proyecto "Nombre"'
-copyright = u'2016, Alumno'
+project = u'Documentación de proyecto "Solo Cultura y Arte"'
+copyright = u'2016, Kevin Velásquez'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
